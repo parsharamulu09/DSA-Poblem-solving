b2bbc9344fa0abc1ca81376e40223aa6ff9492b1
parsharamulu09 <bodduparsharamulu524136@gmail.com>
2025-10-10 05:09:17 +0530

    Add files via upload

---
+++ numberexrecursion.java
@@ -0,0 +1,13 @@
+public class numberexrecursion {
+    static void print1(int n){
+        if(n==5){
+            System.out.println(5);
+            return;
+        }
+        System.out.println(n);
+        print1(n+1);
+    }
+    public static void main(String[] args) {
+        print1(1);
+    }
+}
