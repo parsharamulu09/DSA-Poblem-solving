26d38a00183666352968287629ca32a211d2efa5
parsharamulu09 <bodduparsharamulu524136@gmail.com>
2026-01-19 20:21:08 +0530

    Add files via upload

---
+++ sumofmat.java
@@ -0,0 +1,12 @@
+
+class sumofmat {
+    int sumOfMatrix(int N, int M, int[][] Grid) {
+        int sum=0;
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                sum+=N+M;
+            }
+        }
+        return sum;
+    }
+}
